68bfc5984225b388b77fc7cac346e08fbc706336
piyush-gambhir <90608533+piyush-gambhir@users.noreply.github.com>
2024-08-01 17:58:07 +0530

    chore: Remove unused remotePatterns in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "storage.googleapis.com" },
-      { hostname: "spyne-prod-tech.s3.amazonaws.com" },
-      { hostname: "spyne-static.s3.amazonaws.com" },
-      { hostname: "spyne.s3.amazonaws.com" },
-      { hostname: "spyne-test.s3.amazonaws.com" },
-      { hostname: "spyne-website.s3.amazonaws.com" },
-      { hostname: "spyne-desktop-app.s3.amazonaws.com" },
-      { hostname: "www.spyne.ai" },
-      { hostname: "beta-web.spyne.xyz" },
-      { hostname: "console.spyne.xyz" },
-      { hostname: "console.spyne.ai" },
-      { hostname: "spyne-cliq.s3.ap-south-1.amazonaws.com" },
-      { hostname: "spyne-client-staging-tech.s3.amazonaws.com" },
-      { hostname: "spyne-static.s3.amazonaws.com" },
-      { hostname: "spyne-cliq.s3.amazonaws.com" },
-      { hostname: "spyne-test.s3.amazonaws.com" },
-      { hostname: "spyne-acceleration.s3-accelerate.amazonaws.com" },
-      { hostname: "media.spyneai.com" },
-      { hostname: "spyne.s3.us-east-1.amazonaws.com" },
-      { protocol: "https", hostname: "d20uiuzezo3er4.cloudfront.net" },
-      { hostname: "**" },
-    ],
+    remotePatterns: [],
     formats: ["image/avif", "image/webp"],
   },
 };
